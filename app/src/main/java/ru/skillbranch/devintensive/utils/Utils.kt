fe7d50a5dadc75fd61a51ce5a34d31b218d1ab81
STAYER <istayeri@gmail.com>
2019-07-01 02:10:55 +0700

    Add toInitials and transliteration method for Utils class

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -19,11 +19,76 @@ object Utils {
     }
 
     fun transliteration(payload: String, divider: String = " "): String {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val data = payload.trim().replace(" ", divider)
+        val sb = StringBuilder()
+        data.forEach { sb.append(transliterateChar(it)) }
+        return sb.toString()
+    }
+
+    private fun transliterateChar(char: Char): String {
+        val isUpperCase = char.isUpperCase()
+        val result = when (char.toLowerCase()) {
+            'а' -> "a"
+            'б' -> "b"
+            'в' -> "v"
+            'г' -> "g"
+            'д' -> "d"
+            'е' -> "e"
+            'ё' -> "e"
+            'ж' -> "zh"
+            'з' -> "z"
+            'и' -> "i"
+            'й' -> "i"
+            'к' -> "k"
+            'л' -> "l"
+            'м' -> "m"
+            'н' -> "n"
+            'о' -> "o"
+            'п' -> "p"
+            'р' -> "r"
+            'с' -> "s"
+            'т' -> "t"
+            'у' -> "u"
+            'ф' -> "f"
+            'х' -> "h"
+            'ц' -> "c"
+            'ч' -> "ch"
+            'ш' -> "sh"
+            'щ' -> "sh'"
+            'ъ' -> ""
+            'ы' -> "i"
+            'ь' -> ""
+            'э' -> "e"
+            'ю' -> "yu"
+            'я' -> "ya"
+            else -> char.toString()
+        }
+        return if (isUpperCase) {
+            result.replaceFirst(result[0], result[0].toUpperCase())
+        } else {
+            result
+        }
     }
 
     fun toInitials(firstName: String?, lastName: String?): String? {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val firstInitial = if (firstName.isNullOrBlank()) {
+            null
+        } else {
+            firstName.trim()[0].toUpperCase().toString()
+        }
+
+        val lastInitial = if (lastName.isNullOrBlank()) {
+            null
+        } else {
+            lastName.trim()[0].toUpperCase().toString()
+        }
+
+        return when {
+            firstInitial == null && lastInitial == null -> null
+            firstInitial == null -> lastInitial
+            lastInitial == null -> firstInitial
+            else -> firstInitial + lastInitial
+        }
     }
 
 }
