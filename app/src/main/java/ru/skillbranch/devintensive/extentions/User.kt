b52725dc57ef47a229e6bad85a33590f0d4677c2
STAYER <istayeri@gmail.com>
2019-06-30 14:52:19 +0700

    Create project from video and made few tasks
    Made task (DTO User, Factory).
    Made task (Base Message, Abstract Factory).
    Made task (parseFullName) with tests.

---
+++ app/src/main/java/ru/skillbranch/devintensive/extentions/User.kt
@@ -0,0 +1,32 @@
+package ru.skillbranch.devintensive.extentions
+
+import ru.skillbranch.devintensive.models.User
+import ru.skillbranch.devintensive.models.UserView
+import ru.skillbranch.devintensive.utils.Utils
+import java.util.*
+
+
+fun User.toUserView(): UserView {
+
+    val nickName = Utils.transliteration("$firstName $lastName")
+    val initials = Utils.toInitials(firstName, lastName)
+    val status =
+        when {
+            lastVisit == null -> "Еще ни разу не был"
+            isOnline -> "online"
+            else -> "Последний раз был ${lastVisit!!.humanizeDiff()}"
+        }
+
+    return UserView(
+        id,
+        "$firstName $lastName",
+        nickName,
+        avatar,
+        status,
+        initials
+    )
+}
+
+fun Date.humanizeDiff(date: Date = Date()): String {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+}
