fe463733a0a7e95b5406e29c26534c8cb6a50bbd
STAYER <istayeri@gmail.com>
2019-07-04 02:13:24 +0700

    Fix truncate

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -2,12 +2,15 @@ package ru.skillbranch.devintensive.extensions
 
 
 fun String.truncate(value: Int = 16): String {
-    val trimmed = this.trim()
-    return if (trimmed.length <= value) {
-        trimmed
-    } else {
-        trimmed.slice(0 until value).trim().plus("...")
-    }
+//    val trimmed = this.trim()
+//    return if (trimmed.length <= value) {
+//        trimmed
+//    } else {
+//        trimmed.slice(0 until value).trim().plus("...")
+//    }
+    val trimmedMes = this.trim()
+    val subLine = trimmedMes.substring(0, value + 1).trim()
+    return if (subLine.length < trimmedMes.length) "$subLine..." else subLine
 }
 
 /**
