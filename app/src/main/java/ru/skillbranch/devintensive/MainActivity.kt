2aab811ee67310cb4c9cdfdddf27a179a4cedefc
STAYER <istayeri@gmail.com>
2019-07-13 21:45:27 +0700

    Do code from 3-d lesson

--- app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
@@ -1,12 +1,92 @@
 package ru.skillbranch.devintensive
 
+import android.graphics.Color
+import android.graphics.PorterDuff
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import ru.skillbranch.devintensive.models.Bender
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var benderImage: ImageView
+    lateinit var textTxt: TextView
+    lateinit var messageEt: EditText
+    lateinit var sendBtn: ImageView
+
+    lateinit var benderObj: Bender
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        benderImage = iv_bender
+        textTxt = tv_text
+        messageEt = et_message
+        sendBtn = iv_send.apply {
+            setOnClickListener { onClick(it) }
+        }
+
+        val status = savedInstanceState?.getString("STATUS") ?: Bender.Status.NORMAL.name
+        val question = savedInstanceState?.getString("QUESTION") ?: Bender.Question.NAME.name
+        benderObj = Bender(Bender.Status.valueOf(status), Bender.Question.valueOf(question))
+
+        Log.d("test1", "onCreate, $status $question")
+
+        val (r, g, b) = benderObj.status.color
+        //TODO поменять мод фильтра под пнг
+        benderImage.setColorFilter(Color.rgb(r,g,b), PorterDuff.Mode.SRC_IN)
+
+        textTxt.text = benderObj.askQuestion()
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("test1", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("test1", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("test1", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("test1", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("test1", "onDestroy")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+
+        outState?.putString("STATUS", benderObj.status.name)
+        outState?.putString("QUESTION", benderObj.question.name)
+        Log.d("test1", "onSaveInstanceState ${benderObj.status.name} ${benderObj.question.name}")
+    }
+
+    fun onClick(view: View) {
+        if (view.id == R.id.iv_send) {
+            val (phrase, color) = benderObj.listenAnswer(messageEt.text.toString().toLowerCase())
+            messageEt.setText("")
+            val (r, g, b) = color
+            //TODO поменять мод фильтра под пнг
+            benderImage.setColorFilter(Color.rgb(r,g,b), PorterDuff.Mode.SRC_IN)
+            textTxt.text = phrase
+        }
+    }
+
 }
